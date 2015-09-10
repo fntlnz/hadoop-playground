6b9264f6bbb0d0fbf337be8468172bcdb22042e9
Lorenzo Fontana <fontanalorenzo@me.com>
2015-09-10 23:35:33 +0200

    Unit test reduce

--- wordcount/src/test/java/wtf/fntlnz/wordcount/IntSumReducerTest.java
+++ wordcount/src/test/java/wtf/fntlnz/wordcount/IntSumReducerTest.java
@@ -2,8 +2,26 @@ package wtf.fntlnz.wordcount;
 
 import junit.framework.TestCase;
 
-public class IntSumReducerTest extends TestCase {
-    public void testReduce() {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntSumReducerTest extends TestCase {
+    @Test
+    public void testReduce() throws IOException {
+        IntSumReducer reducer = new IntSumReducer();
+        ReduceDriver<Text, IntWritable, Text, IntWritable> reduceDriver;
+        reduceDriver = ReduceDriver.newReduceDriver(reducer);
+        List<IntWritable> exampleValues = new ArrayList<IntWritable>();
+        exampleValues.add(new IntWritable(10));
+        exampleValues.add(new IntWritable(30));
+        reduceDriver.withInput(new Text("example"), exampleValues);
+        reduceDriver.withOutput(new Text("example"), new IntWritable(40));
+        reduceDriver.runTest();
     }
 }
